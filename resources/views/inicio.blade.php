ddd56c4fe92785aa17aeb0a17389c48daa4968f7
ruedakax <ruedakax@gmail.com>
2020-07-20 16:07:43 -0500

    update

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -6,7 +6,7 @@
 		<meta name="csrf-token" href="{{csrf_token()}}">
         <title>Laravel</title>
         <!-- Styles -->
-		<link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
+		<link rel="stylesheet" href="https://coticar.herokuapp.com/css/app.css">
         <style>
             html, body {
                 background-color: #fff;
@@ -67,5 +67,5 @@
             </div>
         </div>
     </body>
-	<script type="text/javascript" src="{{secure_asset('js/app.js')}}"></script>
+	<script type="text/javascript" src="https://coticar.herokuapp.com/js/app.js"></script>
 </html>
